3ebfd9c9ed4560bd19eb09a8fe1b26d391600b06
khanh ct <KhanhCT>
2021-08-04 14:47:53 +0700

    Update Log
    Signed-off-by: khanh ct <KhanhCT>

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/gin-gonic/gin v1.7.2
 	github.com/google/uuid v1.2.0
+	github.com/sirupsen/logrus v1.8.1
 	gorm.io/gorm v1.21.12
 )
--- sdk/api/api.go
+++ sdk/api/api.go
@@ -2,9 +2,11 @@ package api
 
 import (
 	"errors"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/khanhct/go-lib-core/sdk/response"
+	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 )
 
@@ -14,6 +16,7 @@ type Api struct {
 	Context *gin.Context
 	Orm     *gorm.DB
 	Errors  error
+	Logger  *logrus.Logger
 }
 
 func (e *Api) AddError(err error) {
@@ -53,6 +56,32 @@ func getOrm(c *gin.Context) (*gorm.DB, error) {
 	}
 }
 
+func (e *Api) MakeLogger(f func(c *gin.Context) (*logrus.Logger, error)) *Api {
+	if f == nil {
+		f = getLogger
+	}
+
+	logger, err := f(e.Context)
+	if err != nil {
+		e.AddError(err)
+	}
+	e.Logger = logger
+	return e
+}
+
+func getLogger(c *gin.Context) (*logrus.Logger, error) {
+	ilogger, exist := c.Get("logger")
+	if !exist {
+		return nil, errors.New("logger connect not exist")
+	}
+	switch idb := ilogger.(type) {
+	case *logrus.Logger:
+		return idb, nil
+	default:
+		return nil, errors.New("logger connect not exist")
+	}
+}
+
 func (e Api) Error(code int, err error, msg string) {
 	response.Error(e.Context, code, err, msg)
 }
@@ -67,4 +96,25 @@ func (e Api) PageOK(result interface{}, count int, pageIndex int, pageSize int, 
 
 func (e Api) Custom(data gin.H) {
 	response.Custum(e.Context, data)
+}
+
+func (e Api) GetClient() string {
+	ClientIP := e.Context.ClientIP()
+	//fmt.Println("ClientIP:", ClientIP)
+	RemoteIP, _ := e.Context.RemoteIP()
+	//fmt.Println("RemoteIP:", RemoteIP)
+	ip := e.Context.Request.Header.Get("X-Forwarded-For")
+	if strings.Contains(ip, "127.0.0.1") || ip == "" {
+		ip = e.Context.Request.Header.Get("X-real-ip")
+	}
+	if ip == "" {
+		ip = "127.0.0.1"
+	}
+	if RemoteIP.String() != "127.0.0.1" {
+		ip = RemoteIP.String()
+	}
+	if ClientIP != "127.0.0.1" {
+		ip = ClientIP
+	}
+	return ip
 }
